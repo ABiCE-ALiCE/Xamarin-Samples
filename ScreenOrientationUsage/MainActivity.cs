89357d3ed36c4cbceaf1afe47a70d62ff6a47019
ABiCE.ALiCE <ABiCE.ALiCE@gmail.com>
2016-12-08 21:46:56 +0900

    add Xamarin-Samples/Android/ScreenOrientationUsage

---
+++ ScreenOrientationUsage/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ScreenOrientationUsage
+{
+    [Activity(Label = "ScreenOrientationUsage", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            // Add `android:configChanges="orientation|screenSize"` in Main.axml
+        }
+    }
+}
+
